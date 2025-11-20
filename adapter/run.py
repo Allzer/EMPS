477cf4e3c4717326f782c953389895c7b7f4f0fd
Allzer <dshapkarin@list.ru>
2025-11-20 19:33:36 +0300

    добавил на адаптер функцию отправления данных о системе

--- adapter/run.py
+++ adapter/run.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from fastapi import FastAPI, Request
 import requests
@@ -5,6 +6,19 @@ import uvicorn
 from config import Config
 
 app = FastAPI()
+
+async def create_or_check_system():
+    system_params = {
+        'system_name': 'EMPS SYSTEM',
+        'system_key': 'EMPS_SYSTEM_key',
+    }
+    
+    res = requests.post(
+        url=f'http://0.0.0.0:5000/monitoring/create_system',
+        json=system_params
+    )
+    print(res.text)
+    return res.json()
 
 @app.post('/')
 async def post_ftp_data(request: Request):
@@ -16,4 +30,5 @@ async def post_ftp_data(request: Request):
     return 'ok'
 
 if __name__ == '__main__':
+    asyncio.run(create_or_check_system())
     uvicorn.run('run:app', host='0.0.0.0', port=7000, reload=True)
--- monitoring/src/api/monitoring_api.py
+++ monitoring/src/api/monitoring_api.py
@@ -14,6 +14,11 @@ async def get_info(request: Request):
 @router.post('/')
 async def post_ftp_data(request: Request):
     data = await request.json()
-    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
+    # print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
     
     return 'ok'
+
+@router.post('/create_system')
+async def post_ftp_data(request: Request):
+    data = await request.json()
+    return 'система создана'
