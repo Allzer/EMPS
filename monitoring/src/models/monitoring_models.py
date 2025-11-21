76b909b3ce8609f7103f0af58456fbb911933c6e
Allzer <dshapkarin@list.ru>
2025-11-21 17:59:05 +0300

    добавил таблицу сенсеров

--- monitoring/src/models/monitoring_models.py
+++ monitoring/src/models/monitoring_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Uuid, TEXT
+from sqlalchemy import INTEGER, Column, ForeignKey, Uuid, TEXT
 
 from database import Base
 
@@ -13,4 +13,16 @@ class SystemsModel(Base):
     id = Column(Uuid, primary_key=True)
     
     system_name = Column(TEXT, nullable=False)
-    system_key = Column(TEXT, nullable=False)
+    system_key = Column(TEXT, nullable=False, unique=True)
+
+
+class SensorsModel(Base):
+
+    __tablename__ = 'sensors'
+
+    id = Column(Uuid, primary_key=True)
+    system_id = Column(Uuid, ForeignKey('systems.id'))
+
+    sensor_name = Column(TEXT, nullable=False)
+    state = Column(INTEGER, nullable=False)
+    description = Column(INTEGER, nullable=False)
