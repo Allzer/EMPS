6a273a57222a719faeba34dbbcbdf5f037eb639e
Allzer <dshapkarin@list.ru>
2025-11-26 12:23:34 +0300

    возвращение сенсеров в зависимости от системы

--- monitoring/src/api/monitoring_api.py
+++ monitoring/src/api/monitoring_api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request
 from sqlalchemy import select
 
 from database import SessionDep
-from src.models.monitoring_models import SystemsModel
+from src.models.monitoring_models import SensorsModel, SystemsModel
 from src.api.scripts import add_sensor, check_system, create_system
 
 router = APIRouter(
@@ -33,11 +33,23 @@ async def create_system(request: Request, session : SessionDep):
     query = await session.execute(system)
     query = query.scalars().first()
 
+    sensors = (select(SensorsModel)).where(SensorsModel.system_id == system_id)
+    sensors_query = await session.execute(sensors)
+    sensors_info = sensors_query.scalars().all()
+
+    result_dict = {}
+    for sensor in sensors_info:
+        result_dict.update(
+            {
+                sensor.sensor_name: sensor.id
+            }
+        )
+
     return {
         'system_id': system_id,
         'system_name': query.system_name,
         'system_key': query.system_key,
-        'list_of_sensors': {}
+        'list_of_sensors': result_dict
     }
 
 @router.post('/add_sensors')
